63edc8a75ea917e9ad9f5e2e93b6534f3a97bcfe
shreyanshg2010 <31040579+shreyanshg2010@users.noreply.github.com>
2017-09-21 01:33:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='new value';
-//move image
-var img=document.getElementById('img');
-img.onclick=function(){
-    img.style.marginLeft='100px';
+var button=document.getElementById('counter');
+var counter=0;
+
+button.onclick= function () {
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.tostring();
 }
